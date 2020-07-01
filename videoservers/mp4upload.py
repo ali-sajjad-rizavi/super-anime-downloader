066ebcaacb3b47ee3917acbe885813d70e5ced8c
Ali Sajjad <sasrizavi@gmail.com>
2020-07-01 21:57:12 +0500

    Edited mp4upload generator

--- videoservers/mp4upload.py
+++ videoservers/mp4upload.py
@@ -3,26 +3,21 @@ import requests
 import re as RegExp
 
 
-class Mp4UploadGenerator:
-	def __init__(self, embed_url):
-		self.embed_url = embed_url
-
-	def fetchDownloadLink(self):
-		scripts = BeautifulSoup(requests.get(self.embed_url).text, 'html.parser').find_all('script', type="text/javascript")
-		evalText = [script.text for script in scripts if "|embed|" in script.text][0]
-		#evalText = scripts[len(scripts)-1].text
-		evalItems = evalText.split('|')
-		del evalItems[:evalItems.index('mp4upload')+1]
-		videoID = [a for a in evalItems if len(a)>30][0]
-		#
-		evalItems = evalText.split('|')
-		w3strPossiblesList = [s for s in evalItems if RegExp.match('s\d+$', s) or RegExp.match('www\d+$', s)]
-		w3str = "www"
-		if len(w3strPossiblesList) is not 0:
-			w3str = max(w3strPossiblesList, key=len)
-		#
-		retstr = "https://" + w3str + ".mp4upload.com:" + evalItems[evalItems.index(videoID)+1] + "/d/" + videoID + "/video.mp4"
-		return retstr
+def get_mp4upload_downloadLink(embed_url):
+	scripts = BeautifulSoup(requests.get(embed_url).text, 'html.parser').find_all('script', type="text/javascript")
+	evalText = [script.text for script in scripts if "|embed|" in script.text][0]
+	#evalText = scripts[len(scripts)-1].text
+	evalItems = evalText.split('|')
+	del evalItems[:evalItems.index('mp4upload')+1]
+	videoID = [a for a in evalItems if len(a)>30][0]
+	#
+	evalItems = evalText.split('|')
+	w3strPossiblesList = [s for s in evalItems if RegExp.match('s\d+$', s) or RegExp.match('www\d+$', s)]
+	w3str = "www"
+	if len(w3strPossiblesList) is not 0:
+		w3str = max(w3strPossiblesList, key=len)
+	#
+	return 'https://{}.mp4upload.com:{}/d/{}/video.mp4'.format(w3str, evalItems[evalItems.index(videoID)+1], videoID)
 
 ######
 ######
@@ -34,14 +29,12 @@ def main():
 	print('\t\t=====================')
 	print('\t\t Mp4Upload Generator')
 	print('\t\t=====================')
-	mp4upload_generator = Mp4UploadGenerator(input('\t- Enter Mp4Upload Embed URL: '))
-	download_link = mp4upload_generator.fetchDownloadLink()
 	try:
-		download_link = mp4upload_generator.fetchDownloadLink()
+		download_link = get_mp4upload_downloadLink(input('\t- Enter Mp4Upload Embed URL: '))
 		print('- The generated download link: ', download_link)
 	except Exception as e:
 		print('\tSomething went wrong while generating the Download link!')
-		print('Error says: ', e)
+		print('Error message: ', e)
 
 if __name__ == '__main__':
 	main()
