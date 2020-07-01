dbe6e0e9801f2519074e7ee0f7013f579d9b27e9
Ali Sajjad <sasrizavi@gmail.com>
2020-07-01 23:23:02 +0500

    Updated command-line downloader

--- cli_downloader.py
+++ cli_downloader.py
@@ -1,47 +1,56 @@
-import os
-import subprocess
+import os, subprocess, glob
 
-from anime import Anime
-from episode import Episode
+from gogoanime import AnimeScraper as GogoanimeScraper
+import download_link_builder
+
+
 
 
 class Downloader:
-	def __init__(self, anime):
-		self.anime = anime
+	def __init__(self, anime_dictionary):
+		if os.path.isfile('failed.txt'):
+			os.remove('failed.txt')
+		self.anime_dict = anime_dictionary
 
-	def __downloadEpisode(self, episode):
-		if episode.is_mp4Upload_available() == False:
-			print("\n", "::: COULD NOT FIND ::: EPISODE:-", episode.getTitle(), "| Server=MP4-UPLOAD\n")
-		else:
-			print("============================================================================")
-			print(" DOWNLOADING EPISODE:", episode.getTitle().replace(' ', '_'))
-			print("============================================================================")
-			options = " -x 10 --max-tries=5 --retry-wait=10 --check-certificate=false -d downloaded -o "
-			episode_filename = episode.getTitle().replace(' ', '_') + ".mp4"
-			cmd = "aria2c " + episode.fetchDownloadLink() + options + episode_filename
-			#..........
-			#..........
-			if os.path.isfile("downloaded/" + episode_filename) and not os.path.isfile("downloaded/" + episode_filename + ".aria2"):
-				return
-			#-------------
-			while True:
-				try:
-					if os.name == 'posix': subprocess.call(cmd.split())
-					if not os.name == 'posix': os.system(cmd)
-					break
-				except KeyboardInterrupt: input("\nDownloader is paused. PRESS [ENTER] TO CONTINUE...")
-			#-------------
-			if os.path.isfile("downloaded/" + episode_filename + ".aria2"):
-				self.failed_downloads_file.write(cmd + '\n')
+	def __downloadEpisode(self, filename='episode.mp4', download_link='download-url'):
+		print("============================================================================")
+		print(" DOWNLOADING EPISODE:", filename)
+		print("============================================================================")
+		options = f'-x 10 --max-tries=5 --retry-wait=10 --check-certificate=false -d downloaded -o "{filename}"'
+		cmd = f'aria2c {download_link} {options}'
+		print(cmd)
+		input()
+		#..........
+		if os.path.isfile(f'downloaded/{filename}') and not os.path.isfile(f'downloaded/{episode_filename}.aria2'):
+			return
+		#-------------
+		while True:
+			try:
+				if os.name == 'posix': subprocess.call(cmd.split())
+				if not os.name == 'posix': os.system(cmd)
+				break
+			except KeyboardInterrupt: input("\nDownloader is paused. PRESS [ENTER] TO CONTINUE...")
+		#-------------
+		if os.path.isfile("downloaded/" + filename + ".aria2"):
+			open('failed.txt', 'a').write(cmd + '\n')
 
 	def downloadAnime(self):
-		self.failed_downloads_file = open('failed.txt', 'w', encoding='utf-8')
-		for episode in self.anime.getEpisodeList():
-			self.__downloadEpisode(episode)
-		self.failed_downloads_file.close()
-		self.anime.finalizeAnime()
+		for episodeDict in self.anime_dict['scraped-episodes']:
+			download_link = download_link_builder.get_available_download_link(episodeDict)
+			if download_link == 'unavailable':
+				continue
+			self.__downloadEpisode(filename=episodeDict['episode-title']+'.mp4', download_link=download_link)
+		self.__retryFailedDownloads()
+		if len(glob.glob('downloaded/*.aria2')) != 0:
+			return
+		os.rename('downloaded', self.anime_dict['anime-title'])
 
-	def retryFailedDownloads(self):
+	def __retryFailedDownloads(self):
+		if not os.path.isfile('failed.txt'):
+			return
+		print('-------------------------------------')
+		print('- Retrying failed downloads')
+		print('-------------------------------------')
 		commands = open('failed.txt', 'r').read().strip().split('\n')
 		for command in commands:
 			while True:
@@ -51,6 +60,10 @@ class Downloader:
 					break
 				except KeyboardInterrupt: input('\nDownloader is paused. PRESS [ENTER] TO CONTINUE...')
 
+	def __del__(self):
+		if os.path.isfile('failed.txt'):
+			os.remove('failed.txt')
+
 
 
 ###----------------###
@@ -58,17 +71,16 @@ class Downloader:
 ###----------------###
 
 def main():
-    print("\t\t|==================|")
-    print("\t\t| ANIME DOWNLOADER |")
-    print("\t\t|==================|\n")
+    print("\t\t|======================|")
+    print("\t\t| CLI ANIME DOWNLOADER |")
+    print("\t\t|======================|\n")
     #
-    theanime = Anime(input(" - Enter Anime main-page URL: "))
-    print("\t -FOUND:", theanime.getTotalEpisodeCount(), " Episodes in TOTAL!\n")
-    theanime.collectEpisodes(int(input("\t - Start From Episode: ")), int(input("\t - End At Episode: ")))
+    anime_scraper = GogoanimeScraper(input(" - Enter Anime main-page URL: "))
+    print("\t -FOUND:", anime_scraper.episode_count, " Episodes in TOTAL!\n")
+    anime_scraper.scrapeEpisodes(int(input("\t - Start From Episode: ")), int(input("\t - End At Episode: ")))
+    #
     print("\nStarting Download using aria2...\n")
-    theanime.displayEpisodes()
-    #
-    downloader = Downloader(theanime)
+    downloader = Downloader(anime_scraper.dataDict)
     downloader.downloadAnime()
     print("=======================================================")
     print("-------------------- COMPLETED !!! --------------------")
