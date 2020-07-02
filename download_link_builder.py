ae796b3bd53da16a8d1a88ed645927a7a9a48ae7
Ali Sajjad <sasrizavi@gmail.com>
2020-07-02 11:16:01 +0500

    Added Vidcdn server as an available option

--- download_link_builder.py
+++ download_link_builder.py
@@ -1,9 +1,12 @@
 from videoservers import mp4upload
+from videoservers import vidcdn
 
 
 def get_available_download_link(episode_dict):
 	if 'mp4' in episode_dict['embed-servers'].keys():
 		return mp4upload.get_mp4upload_download_link(episode_dict['embed-servers']['mp4'])
+	if 'vidcdn' in episode_dict['embed-servers'].keys():
+		return vidcdn.get_vidcdn_download_link(episode_dict['embed-servers']['vidcdn'])
 	return 'unavailable'
 
 
