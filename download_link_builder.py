cc55f1b57c8f3043d51285bcade31f3868c09c9f
Ali Sajjad Rizavi <sasrizavi@gmail.com>
2021-03-22 01:25:53 +0500

    New video server mixdrop to be used on first priority

--- download_link_builder.py
+++ download_link_builder.py
@@ -1,12 +1,21 @@
 from videoservers import mp4upload
 from videoservers import vidcdn
+from videoservers import mixdrop
 
 
 def get_available_download_link(episode_dict):
 	if 'mp4' in episode_dict['embed-servers'].keys():
-		return mp4upload.get_mp4upload_download_link(episode_dict['embed-servers']['mp4'])
+		download_link = mp4upload.get_mp4upload_download_link(episode_dict['embed-servers']['mp4'])
+		if download_link is not None:
+			return download_link
 	if 'vidcdn' in episode_dict['embed-servers'].keys():
-		return vidcdn.get_vidcdn_download_link(episode_dict['embed-servers']['vidcdn'])
+		download_link = vidcdn.get_vidcdn_download_link(episode_dict['embed-servers']['vidcdn'])
+		if download_link is not None:
+			return download_link
+	if 'mixdrop' in episode_dict['embed-servers'].keys():
+		download_link = mixdrop.get_download_link(episode_dict['embed-servers']['mixdrop'])
+		if download_link is not None:
+			return mixdrop.get_download_link(episode_dict['embed-servers']['mixdrop'])
 	return 'unavailable'
 
 
