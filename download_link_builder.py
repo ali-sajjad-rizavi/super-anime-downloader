6bf03a7f0a388f1edab6d0ef1ae3b7c6c6fe1cba
Ali <sasrizavi@gmail.com>
2021-05-07 01:50:55 +0500

    Reformatting

--- download_link_builder.py
+++ download_link_builder.py
@@ -5,31 +5,31 @@ from videoservers import streamtape
 
 
 def get_available_download_link(episode_dict):
-	''' Mixdrop '''
+	# Mixdrop
 	if 'mixdrop' in episode_dict['embed-servers'].keys():
 		download_link = mixdrop.get_download_link(episode_dict['embed-servers']['mixdrop'])
 		if download_link is not None:
 			return download_link
 
-	''' Streamtape '''
+	# Streamtape
 	if 'streamtape' in episode_dict['embed-servers'].keys():
 		download_link = streamtape.get_download_link(episode_dict['embed-servers']['streamtape'])
 		if download_link is not None:
 			return download_link
 
-	''' Mp4Upload '''
+	# Mp4Upload
 	if 'mp4upload' in episode_dict['embed-servers'].keys():
 		download_link = mp4upload.get_download_link(episode_dict['embed-servers']['mp4upload'])
 		if download_link is not None:
 			return download_link
 
-	''' Mp4Upload '''
+	# Mp4Upload
 	if 'mp4' in episode_dict['embed-servers'].keys():
 		download_link = mp4upload.get_download_link(episode_dict['embed-servers']['mp4'])
 		if download_link is not None:
 			return download_link
 
-	''' VidCDN '''
+	# VidCDN
 	if 'vidcdn' in episode_dict['embed-servers'].keys():
 		download_link = vidcdn.get_download_link(episode_dict['embed-servers']['vidcdn'])
 		if download_link is not None:
--- videoservers/mixdrop.py
+++ videoservers/mixdrop.py
@@ -1,33 +1,29 @@
 import requests
 import re
 
-
-my_headers = {}
-my_headers['user-agent'] = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-
+my_headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/83.0.4103.116 Safari/537.36 '
+}
 
 
 def get_download_link(embed_url):
-	try:
-		response = requests.get(embed_url, headers=my_headers)
-		if 'tb error' in response.text:
-			return None
-		video_details = re.findall('MDCore\|\|.+\|poster', response.text)[0].split('|')
-		''' Example:
-		//s-delivery14.mxdcontent.net/v/59f0fcbf63176bff9792eb2b2c5218dd.mp4?
-		s=dZk3d430Oi1bSCD5HfvMbA&e=1616377672&_t=1616359487
-		'''
-		download_link = 'https://{2}-{3}.{6}.{7}/v/{4}.{5}?{2}={9}&e={17}&{16}={18}'.format(*video_details)
-		return download_link
-	except Exception as e:
-		print(e)
-		return None
+    try:
+        response = requests.get(embed_url, headers=my_headers)
+        if 'tb error' in response.text:
+            return None
+        video_details = re.findall('MDCore\|\|.+\|poster', response.text)[0].split('|')
+        # Example:
+        # //s-delivery14.mxdcontent.net/v/59f0fcbf63176bff9792eb2b2c5218dd.mp4?
+        # s=dZk3d430Oi1bSCD5HfvMbA&e=1616377672&_t=1616359487
+        download_link = 'https://{2}-{3}.{6}.{7}/v/{4}.{5}?{2}={9}&e={17}&{16}={18}'.format(*video_details)
+        return download_link
+    except Exception as e:
+        print(e)
+        return None
 
-
-
-######
-######
-######
 
 if __name__ == '__main__':
-	print(get_download_link('https://mixdrop.co/e/7rgov1qpuk4rq7'))
+    # print(get_download_link('https://mixdrop.co/e/7rgov1qpuk4rq7'))
+    mixdrop_embed_url = input('Enter mixdrop embed URL: ')
+    print(get_download_link(mixdrop_embed_url))
