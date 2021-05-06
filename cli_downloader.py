204ad6a67a54c002ee7a06cf24a1bab26842d45f
Ali <sasrizavi@gmail.com>
2021-05-07 03:49:48 +0500

    Added docstrings and type hints + Black formatting

--- cli_downloader.py
+++ cli_downloader.py
@@ -5,20 +5,54 @@ import download_link_builder
 
 
 class Downloader:
-    def __init__(self, anime_dictionary):
-        if os.path.isfile('failed.txt'):
-            os.remove('failed.txt')
-        self.anime_dict = anime_dictionary
+    def __init__(self, anime_dict: dict):
+        """Constructor of Downloader class"""
+        if os.path.isfile("failed.txt"):
+            os.remove("failed.txt")
+        self.anime_dict = anime_dict
 
-    def __downloadEpisode(self, filename='episode.mp4', download_link='download-url'):
+    def download_anime(self):
+        """
+        Downloads all episodes which were scraped
+        """
+        for episodeDict in self.anime_dict["scraped-episodes"]:
+            download_link = download_link_builder.get_available_download_link(episodeDict)
+
+            # Skip if episode download URL is not available
+            if download_link == "unavailable":
+                continue
+
+            # TODO: Implement a technique to download .m3u8 files.
+            file_extension = "m3u8" if "m3u8" in download_link else "mp4"
+
+            ep_title = episodeDict["episode-title"]
+            Downloader.download_episode(
+                filename=f"{ep_title}.{file_extension}",
+                download_link=download_link,
+            )
+
+        self.retry_failed_download_commands()
+
+    @staticmethod
+    def download_episode(filename: str, download_link: str):
+        """
+        Downloads a single episode using aria2 downloader
+
+        :param filename: Name of video file to be downloaded
+        :param download_link: Download URL of video
+        """
         print("============================================================================")
-        print(" DOWNLOADING EPISODE:", filename)
+        print(f" DOWNLOADING EPISODE: {filename}")
         print("============================================================================")
+
+        # Command to use on terminal/cmd for downloading the video using aria2
         options = f'-x 10 --max-tries=5 --retry-wait=10 --check-certificate=false -d downloaded -o "{filename}"'
         cmd = f'aria2c "{download_link}" {options}'
 
         # Skip this episode download if the download is already completed
-        if os.path.isfile(f'downloaded/{filename}') and not os.path.isfile(f'downloaded/{filename}.aria2'):
+        if os.path.isfile(f"downloaded/{filename}") and not os.path.isfile(
+            f"downloaded/{filename}.aria2"
+        ):
             return
 
         # Catch Ctrl+C to pause the download, then resume download on pressing ENTER
@@ -30,44 +64,40 @@ class Downloader:
                 input("\nDownloader is paused. PRESS [ENTER] TO CONTINUE...")
 
         # If at the end, the .aria2 file for this episode still exists, it means download
-        # was failed at some point.
+        # was failed at some point. So add the command to "failed.txt" file.
         if os.path.isfile(f"downloaded/{filename}.aria2"):
-            open('failed.txt', 'a').write(cmd + '\n')
+            open("failed.txt", "a").write(cmd + "\n")
 
-    def download_anime(self):
-        for episodeDict in self.anime_dict['scraped-episodes']:
-            download_link = download_link_builder.get_available_download_link(episodeDict)
-            if download_link == 'unavailable':
-                continue
-
-            extension = '.mp4'
-            if '.m3u8' in download_link:
-                extension = '.m3u8'
-            self.__downloadEpisode(filename=episodeDict['episode-title'] + extension, download_link=download_link)
-        self.__retryFailedDownloads()
-
-    def __retryFailedDownloads(self):
-        if not os.path.isfile('failed.txt'):
+    @staticmethod
+    def retry_failed_download_commands():
+        """
+        Retries all failed downloads by running the failed commands in "failed.txt" file.
+        """
+        if not os.path.isfile("failed.txt"):
             return
-        print('-------------------------------------')
-        print('- Retrying failed downloads')
-        print('-------------------------------------')
-        commands = open('failed.txt', 'r').read().strip().split('\n')
-        for command in commands:
+
+        print("-------------------------------------")
+        print("- Retrying failed downloads")
+        print("-------------------------------------")
+
+        failed_commands = open("failed.txt", "r").read().strip().split("\n")
+
+        for cmd in failed_commands:
+            # Catch Ctrl+C to pause the download, then resume download on pressing ENTER
             while True:
                 try:
-                    os.system(command)
+                    os.system(cmd)
                     break
                 except KeyboardInterrupt:
-                    input('\nDownloader is paused. PRESS [ENTER] TO CONTINUE...')
+                    input("\nDownloader is paused. PRESS [ENTER] TO CONTINUE...")
 
     def __del__(self):
-        if len(glob.glob('downloaded/*.aria2')) != 0:
+        if len(glob.glob("downloaded/*.aria2")) != 0:
             return
-        os.rename('downloaded', self.anime_dict['anime-title'])
+        os.rename("downloaded", self.anime_dict["anime-title"])
         # ---
-        if os.path.isfile('failed.txt'):
-            os.remove('failed.txt')
+        if os.path.isfile("failed.txt"):
+            os.remove("failed.txt")
 
 
 # Example: https://www19.gogoanime.io/category/makura-no-danshi
@@ -77,22 +107,25 @@ def main():
     print("\t\t|======================|\n")
     #
     searchInput = input(" - Enter Anime name/URL: ")
-    if 'gogoanime' in searchInput:
+    if "gogoanime" in searchInput:
         anime_scraper = GogoanimeScraper(searchInput)
     else:
-        print('\n\tResults:\n')
-        anime_search_results = GogoanimeScraper.searchAnime(query=searchInput)
-        [print(f'\t {i + 1}) {anime_search_results[i][0]}') for i in range(len(anime_search_results))]
-        selected_index = int(input('\n- Select your option: ')) - 1
+        print("\n\tResults:\n")
+        anime_search_results = GogoanimeScraper.search_anime(query=searchInput)
+        [
+            print(f"\t {i + 1}) {anime_search_results[i][0]}")
+            for i in range(len(anime_search_results))
+        ]
+        selected_index = int(input("\n- Select your option: ")) - 1
         anime_scraper = GogoanimeScraper(anime_search_results[selected_index][1])
     #
     print("\t -FOUND:", anime_scraper.episode_count, " Episodes in TOTAL!\n")
-	# -----
+    # -----
     start_ep = int(input("\t - Start From Episode: "))
     end_ep = int(input("\t - End At Episode: "))
-    print('----------------------------------------------------------------------------------')
-    print(f'- Scraping episode video links from {start_ep} to {end_ep}, wait for a while...')
-    anime_scraper.scrapeEpisodes(start=start_ep, end=end_ep)
+    print("----------------------------------------------------------------------------------")
+    print(f"- Scraping episode video links from {start_ep} to {end_ep}, wait for a while...")
+    anime_scraper.scrape_episodes(start=start_ep, end=end_ep)
     #
     print("\nStarting Download using aria2...\n")
     downloader = Downloader(anime_scraper.dataDict)
@@ -102,8 +135,8 @@ def main():
     print("=======================================================")
     #
     print("Done!")
-    input('- Press [ENTER] to quit...')
+    input("- Press [ENTER] to quit...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- gogoanime.py
+++ gogoanime.py
@@ -1,73 +1,128 @@
-import requests, json, os
+from typing import List
+
+import requests, json
 from bs4 import BeautifulSoup
-import re as RegExp
+import re
 
-
-my_headers = {}
-my_headers['user-agent'] = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-
+my_headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/83.0.4103.116 Safari/537.36 "
+}
 
 
 class AnimeScraper:
-	def __init__(self, url):
-		animeSoup = BeautifulSoup(requests.get(url, headers=my_headers).text, 'html.parser')
-		animeID = animeSoup.find(id='movie_id')['value']
-		animeAlias = animeSoup.find(id='alias_anime')['value']
-		animeLastEp = animeSoup.find(id='episode_page').find_all('a')[-1]['ep_end']
-		#------------------------------
-		ajax_url = f'https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=0&ep_end={animeLastEp}&id={animeID}&default_ep=0&alias={animeAlias}'
-		self.dataDict = {}
-		#re.sub('[<>?":/|]', '', x)
-		self.dataDict['anime-title'] = RegExp.sub('[<>?":/|]', '', animeSoup.title.text.replace('at Gogoanime', '').replace('Watch ', '').strip())
-		self.dataDict['anime-url'] = url
-		ajaxSoup = BeautifulSoup(requests.get(ajax_url, headers=my_headers).text, 'html.parser')
-		self.episode_count = len(ajaxSoup.find_all('a'))
-		#------------------------------
-		self.dataDict['episodes'] = []
-		for li in reversed(ajaxSoup.find_all('li')):
-			episodeDict = {}
-			#re.sub('[<>?":/|]', '', x)
-			#episodeDict['episode-title'] = self.dataDict['anime-title'] + ' - ' + ' '.join(li.text.split())
-			episodeDict['episode-title'] = RegExp.sub('[<>?":/|]', '', '{} - {}'.format(self.dataDict['anime-title'], ' '.join(li.text.split())))
-			episodeDict['episode-url'] = 'https://www.gogoanime.so{}'.format(li.find('a')['href'].strip())
-			self.dataDict['episodes'].append(episodeDict)
+    def __init__(self, url: str):
+        """
+        Constructor for AnimeScraper.
+        Scrapes some information about anime, which is used later for further
+        scraping and downloading.
 
-	def scrapeEpisodes(self, start=1, end=1):
-		self.dataDict['scraped-episodes'] = []
-		for episodeDict in self.dataDict['episodes'][start-1:end]:
-			scraped_episodeDict = episodeDict
-			soup = BeautifulSoup(requests.get(episodeDict['episode-url'], headers=my_headers).text, 'html.parser')
-			serversList = soup.find('div', {'class':'anime_muti_link'}).find_all('li')[1:]
-			scraped_episodeDict['embed-servers'] = {}
-			for li in serversList:
-				embedUrl = li.find('a')['data-video']
-				if not 'https:' in embedUrl:
-					embedUrl = f'https:{embedUrl}'
-				scraped_episodeDict['embed-servers'][li['class'][0]] = embedUrl
-			#------
-			self.dataDict['scraped-episodes'].append(scraped_episodeDict)
-			print('- Collected:', scraped_episodeDict['episode-title'])
+        :param url: Anime URL
+        """
+        # Soup object for scraping
+        anime_soup = BeautifulSoup(requests.get(url, headers=my_headers).text, "html.parser")
 
-	def saveJSON(self, filename='anime.json'):
-		open(filename, 'w', encoding='utf-8').write(json.dumps(self.dataDict, indent=4, sort_keys=True, ensure_ascii=False))
+        # GET request parameters for ajax URL
+        id_ = anime_soup.find(id="movie_id")["value"]
+        alias = anime_soup.find(id="alias_anime")["value"]
+        last_ep = anime_soup.find(id="episode_page").find_all("a")[-1]["ep_end"]
 
-	# STATIC METHOD
-	def searchAnime(query='anime name'):
-		response_text = requests.get('https://www.gogoanime.so/search.html?keyword={}'.format(query.replace(' ', '%20'))).text
-		p_results = BeautifulSoup(response_text, 'html.parser').find('ul', class_='items').find_all('p', class_='name')[:4]
-		paired_results = [(p.find('a')['title'], 'https://www.gogoanime.so{}'.format(p.find('a')['href'])) for p in p_results]
-		return paired_results # (title, url) pair list is returned
+        # We collect the list of all episodes and their links using this URL
+        ajax_url = "https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=0&default_ep=0" \
+                   f"&ep_end={last_ep}&id={id_}&alias={alias}"
+
+        # This dictionary will later contain episodes information as well
+        self.dataDict = {
+            "anime-title": re.sub(
+                '[<>?":/|]',
+                "",
+                anime_soup.title.text.replace("at Gogoanime", "").replace("Watch ", "").strip(),
+            ),
+            "anime-url": url,
+        }
+
+        ajax_soup = BeautifulSoup(requests.get(ajax_url, headers=my_headers).text, "html.parser")
+        self.episode_count = len(ajax_soup.find_all("a"))
+
+        # Collect information of all episodes for further scraping later
+        self.dataDict["episodes"] = []
+        for li in reversed(ajax_soup.find_all("li")):
+            episode_dict = {
+                "episode-title": re.sub(
+                    '[<>?":/|]',
+                    "",
+                    "{} - {}".format(self.dataDict["anime-title"], " ".join(li.text.split())),
+                ),
+                "episode-url": "https://www.gogoanime.so{}".format(li.find("a")["href"].strip()),
+            }
+            self.dataDict["episodes"].append(episode_dict)
+
+    def scrape_episodes(self, start: int, end: int):
+        """
+        Scrapes episode data including embed video server URLs and store in the anime
+        dictionary.
+
+        :param start: Episode number to start from
+        :param end: Episode number to end at
+        """
+        self.dataDict["scraped-episodes"] = []
+        for episode_dict in self.dataDict["episodes"][start - 1:end]:
+            scraped_episode_dict = episode_dict
+            soup = BeautifulSoup(
+                requests.get(episode_dict["episode-url"], headers=my_headers).text, "html.parser"
+            )
+            servers_list = soup.find("div", {"class": "anime_muti_link"}).find_all("li")[1:]
+            scraped_episode_dict["embed-servers"] = {}
+            for li in servers_list:
+                embed_url = li.find("a")["data-video"]
+
+                # Sometimes, the "https:" part is excluded from URL
+                if "https:" not in embed_url:
+                    embed_url = f"https:{embed_url}"
+
+                scraped_episode_dict["embed-servers"][li["class"][0]] = embed_url
+
+            self.dataDict["scraped-episodes"].append(scraped_episode_dict)
+            print("- Collected:", scraped_episode_dict["episode-title"])
+
+    def save_json(self, filename: str):
+        """
+        Saves anime data in a JSON file, from the dictionary object.
+
+        :param filename: Name for the file to be saved
+        """
+        open(filename, "w", encoding="utf-8").write(
+            json.dumps(self.dataDict, indent=4, sort_keys=True, ensure_ascii=False)
+        )
+
+    @staticmethod
+    def search_anime(q: str) -> List[tuple]:
+        """
+        Searches anime from website using the given keyword
+
+        :param q: Anime name to search
+        :return: List of tuples containing title of anime and it's URL
+        """
+        response_text = requests.get(
+            "https://www.gogoanime.so/search.html?keyword={}".format(q.replace(" ", "%20"))
+        ).text
+
+        p_results = (
+            BeautifulSoup(response_text, "html.parser")
+            .find("ul", class_="items")
+            .find_all("p", class_="name")[:4]
+        )
+
+        paired_results = [
+            (p.find("a")["title"], "https://www.gogoanime.so{}".format(p.find("a")["href"]))
+            for p in p_results
+        ]
+
+        return paired_results
 
 
-######
-######
-######
-
-def main():
-	anime_scraper = AnimeScraper(input('Enter Anime URL: '))
-	anime_scraper.scrapeEpisodes(start=1, end=1)
-	anime_scraper.saveJSON()
-	print('- Saved JSON file!')
-
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    anime_scraper = AnimeScraper(input("Enter Anime URL: "))
+    anime_scraper.scrape_episodes(start=1, end=1)
+    anime_scraper.save_json(filename='anime.json')
+    print("- Saved JSON file!")
